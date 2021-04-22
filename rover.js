434c889a14f545df34cac0efd0165b5a0319a22c
nletcher <>
2021-04-22 15:47:57 +0000

    updated results array

--- rover.js
+++ rover.js
@@ -8,19 +8,15 @@ class Rover {
   receiveMessage(message) {
 
     for (let i = 0; i < message.commands.length; i++) {
-
       if (message.commands[i].commandType === 'MODE_CHANGE') {
         this.mode = message.commands[i].value;
       }
 
-      if (message.commands[i].commandType === 'MOVE') {
-        if (this.mode = 'NORMAL') {
+      if (this.mode === 'NORMAL') {
+        if (message.commands[i].commandType === 'MOVE') {
           this.position = message.commands[i].value;
-        } else if (this.mode = 'LOW_POWER') {
-          this.position != message.commands[i].value;
         }
       }
-
     };
 
     let returnObject = {
@@ -30,15 +26,19 @@ class Rover {
         //0: MOVE
         {completed: true, roverStatus: {position: this.position}},
         //1: STATUS_CHECK
-        {completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}},
+        {roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}},
         //2: MODE_CHANGE
-        {completed: true, roverStatus: {mode: this.mode}}
+        {completed: true, roverStatus: {mode: this.mode}},
+        //3: LOW_POWER
+        {completed: false, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}},
+        //4: NORMAL
+        {completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}}
         ]
     };
 
     if (message.commands.commandType === 'MOVE') {
       if (this.mode = 'LOW_POWER') {
-        returnObject.results[0].completed = false;
+        returnObject.results[0].completed = returnObject.results[3].completed;
       }
     }
 
